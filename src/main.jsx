879f47889f038c1105d67156cc0010de87940ccc
softbryanorg-star <softbryan.org@gmail.com>
2026-04-02 19:30:53 -0600

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </HelmetProvider>
 );
