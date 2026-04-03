75b731d35f7cdc96e3af7b13e637beb6c5e7346a
softbryanorg-star <softbryan.org@gmail.com>
2026-04-03 14:25:07 -0600

    Fix ReactDOM import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HelmetProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </HelmetProvider>
 );
